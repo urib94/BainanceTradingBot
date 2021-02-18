3a25a02a3fd88e8eb44dccc07fea8f6b0dbb63f8
omriattal <omriattal@gmail.com>
2021-02-18 20:13:50 +0200

    refactored stuff in rtd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import com.binance.client.RequestOptions;
 import com.binance.client.SubscriptionClient;
+import com.binance.client.SyncRequestClient;
 import com.binance.client.model.enums.CandlestickInterval;
 
 import java.util.concurrent.ExecutorService;
@@ -8,17 +9,25 @@ import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args){
-        RealTimeData rtd = new RealTimeData("btcusdt", CandlestickInterval.ONE_MINUTE, 40);
         ExecutorService executorService = Executors.newFixedThreadPool(3);
-        SubscriptionClient client = SubscriptionClient.create(PrivateConfig.API_KEY, PrivateConfig.SECRET_KEY);
-        client.subscribeCandlestickEvent("btcusdt", CandlestickInterval.ONE_MINUTE, ((event) -> {
+        RealTimeData rtd = new RealTimeData("btcusdt", CandlestickInterval.ONE_MINUTE, 40);
+        RequestOptions options = new RequestOptions();
+        SyncRequestClient syncRequestClient = SyncRequestClient.create(PrivateConfig.API_KEY, PrivateConfig.SECRET_KEY, options);
+        SubscriptionClient subscriptionClient = SubscriptionClient.create(PrivateConfig.API_KEY, PrivateConfig.SECRET_KEY);
+        String listenKey = syncRequestClient.startUserDataStream();
+
+        subscriptionClient.subscribeCandlestickEvent("btcusdt", CandlestickInterval.ONE_MINUTE, ((event) -> {
             rtd.updateData(event);
             executorService.execute(()-> {
                 System.out.println("omri the king");
             });
 
-        }), null);
-        //client.unsubscribeAll();
+        }), System.out::println);
+        subscriptionClient.subscribeUserDataEvent(listenKey, (event) -> {
+            System.out.println(event);
+        });
+
+        //subscriptionClient.unsubscribeAll();
     }
 }
 
