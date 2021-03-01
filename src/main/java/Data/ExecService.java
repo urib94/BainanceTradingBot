f3a5dcf20ceb5fba90e69afab5f0c93261b83986
yonathan95 <yonathan.wolloch@gmail.com>
2021-03-01 19:32:54 +0200

    good luck!

---
+++ src/main/java/Data/ExecService.java
@@ -0,0 +1,22 @@
+package Data;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecService {
+    private ExecutorService executorService;
+
+    private static class ExecServiceHolder {
+        private static ExecService execService = new ExecService();
+    }
+    private ExecService() {
+        executorService = Executors.newFixedThreadPool(Config.THREAD_NUM);
+    }
+    public ExecutorService getExecutorService() {
+        return executorService;
+    }
+
+    public static ExecService getExecService() {
+        return ExecServiceHolder.execService;
+    }
+}
---
+++ src/main/java/Data/SubClient.java
@@ -0,0 +1,21 @@
+package Data;
+
+import com.binance.client.api.SubscriptionClient;
+
+public class SubClient {
+    private SubscriptionClient subscriptionClient;
+
+    private static class SubClientHolder {
+        private static SubClient subClient = new SubClient();
+    }
+    private SubClient() {
+        subscriptionClient = SubscriptionClient.create(Config.API_KEY, Config.SECRET_KEY);
+    }
+
+    public static SubClient getSubClient(){
+        return SubClientHolder.subClient;
+    }
+    public SubscriptionClient getSubscriptionClient() {
+        return subscriptionClient;
+    }
+}
