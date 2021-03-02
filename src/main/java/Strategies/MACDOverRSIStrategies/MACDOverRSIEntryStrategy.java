7b74f8445b7f17031dfa0e957dee276668ac1bcf
yonathan95 <yonathan.wolloch@gmail.com>
2021-03-02 20:20:23 +0200

    fixed RestApiRequestImpl

--- src/main/java/Strategies/MACDOverRSIStrategies/MACDOverRSIEntryStrategy.java
+++ src/main/java/Strategies/MACDOverRSIStrategies/MACDOverRSIEntryStrategy.java
@@ -22,13 +22,20 @@ public class MACDOverRSIEntryStrategy implements EntryStrategy {
         double prevPrevMacdOverRsiValue = realTimeData.getMacdOverRsiValueAtIndex(realTimeData.getLastCloseIndex()-2);
 
         if (currentMacdOverRsiValue > MACDOverRSIConstants.MACD_OVER_RSI_ENTRY_THRESHOLD && prevMacdOverRsiValue < MACDOverRSIConstants.MACD_OVER_RSI_ENTRY_THRESHOLD) {
+            //TODO: buy stuff
+
+
             return null;
-        } else {
+        }
+        if (realTimeData.getMacdOverRsiSignalLineValueAtIndex(realTimeData.getLastCloseIndex()) < 0) {
             boolean rule1 = currentMacdOverRsiValue < 0;
             boolean rule2 = Math.abs(currentMacdOverRsiValue) < Math.abs(prevMacdOverRsiValue);
             boolean rule3 = Math.abs(prevMacdOverRsiValue) < Math.abs(prevPrevMacdOverRsiValue);
             if (rule1 && rule2 && rule3) {
                 //TODO: buy stuff
+
+
+
                 return null;
             }
         }
