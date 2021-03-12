657d9dec88ed1868e018ccf2ddbe8064c1483f1f
yonathan95 <yonathan.wolloch@gmail.com>
2021-03-12 23:39:53 +0200

    fixed issues

--- src/main/java/strategies/macdOverRSIStrategies/MACDOverRSIConstants.java
+++ src/main/java/strategies/macdOverRSIStrategies/MACDOverRSIConstants.java
@@ -19,8 +19,8 @@ public class MACDOverRSIConstants {
     public static final int RSI_CANDLE_NUM = 9;
     public static final int SMA_CANDLE_NUM = 100;
     public static final double MACD_OVER_RSI_EXIT_SELLING_PERCENTAGE = 100;
-    public static final double POSITIVE_TRAILING_PERCENTAGE = 0.15;
-    public static final double CONSTANT_TRAILING_PERCENTAGE = 0.25;
+    public static final double POSITIVE_TRAILING_PERCENTAGE = 0.2;
+    public static final double CONSTANT_TRAILING_PERCENTAGE = 0.3;
     public static final double LONG_EXIT2_OPEN_THRESHOLD = -0.05;
     public static final double SHORT_EXIT2_OPEN_THRESHOLD = 0.05;
     public static final int STANDARD_DEVIATION_CANDLES = 20;
