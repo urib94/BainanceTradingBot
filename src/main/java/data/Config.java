c3e18369d4867ee31224ebbbdfa2390966b87899
urib94 <61322642+urib94@users.noreply.github.com>
2021-06-18 18:12:58 +0300

    Update Config.java

--- src/main/java/data/Config.java
+++ src/main/java/data/Config.java
@@ -7,8 +7,8 @@ public class Config {
 	public static final double DOUBLE_ONE = 1.0;
     public static final double ONE_HUNDRED = 100.0;
     public static final String BASE_SYMBOL = "bnbusdt";
-    public static String API_KEY = "h4AdqbWrKlF5wOxWmbNTKLq7IpDReZOvVXeOujGwUfU0HjXdIejUmqKvoVLbcXJ6";
-	public static String SECRET_KEY = "7qqNLwB7ZXiZu2nBJhrdv7ARVpNDhImNA1Rj1oXR6eGjZFkGsRC9ayEO7HLYVm6w";
+    public static String API_KEY = "";
+	public static String SECRET_KEY = "";
 	public static final int THREAD_NUM = 6;
 	public static final String SYMBOL = "ethusdt";
 	public static final int CANDLE_NUM = 1499;
